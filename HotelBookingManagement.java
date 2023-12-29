9bb7e47eb8f30c177bd6a1168313758c9a0bf16a
udayt <udayteja115@gmail.com>
2023-12-28 16:29:40 -0800

    Hotel Booking management program Day 1

---
+++ HotelBookingManagement.java
@@ -0,0 +1,10 @@
+public class HotelBookingManagement {
+    public static void main(String[] args){
+        System.out.println("Welcome to our Hotel!");
+        System.out.println("Please select your preferred choice:");
+        System.out.println("\t1.Register Account");
+        System.out.println("\t2.Book Reservation");
+        System.out.println("\t3.Generate Bill");
+        System.out.println("\t4.Give Feedback");
+    }
+}
